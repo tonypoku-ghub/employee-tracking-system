9b4a2d8d33bbcfb319c9175326bc028bc7104152
Anthony Poku <tonypoku@gmail.com>
2022-06-25 10:06:39 -0500

    processed questions up to add_department with no major visuals

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE company_db;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
-  dept_name VARCHAR(30) NOT NULL,
+  name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
@@ -13,9 +13,9 @@ CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
-  dept_id INT NOT NULL,
+  department_id INT NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (dept_id)
+  FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE CASCADE
 );
@@ -24,7 +24,10 @@ CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT, 
+  role_id INT NOT NULL, 
   manager_id INT,
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE CASCADE
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,33 @@
-INSERT INTO favorite_books (book_name)
-VALUES ("The Great Gatsby"),
-       ("Huckleberry Finn"),
-       ("100 Years of Solitude"),
-       ("Things Fall Apart"),
-       ("Crime and Punishment"),
-       ("Moby Dick"),
-       ("Decameron");
-       
+DELETE FROM department;
+
+INSERT INTO department (id, name)
+VALUES (2, "Engineering"),
+      (3, "Finance"),
+      (4,"Legal"),
+      (1, "Sales");
+      
+
+DELETE FROM role;
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, "Sales Lead", "100000", 1),
+      (2, "Sales Person", "80000", 1),
+      (3, "Lead Engineer", "150000", 2),
+      (4, "Software Engineer", "120000", 2),
+      (5, "Account Manager", "160000", 3),
+      (6, "Accountant", "125000", 3),
+      (7, "Legal Team Lead", "250000", 4),
+      (8, "Lawyer", "190000", 4);
+
+DELETE FROM employee;
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1, "John", "Doe",  1, NULL),
+      (2, "Mike", "Chan",  2, 1),
+      (3, "Ashley", "Rodriguez",  3, NULL),
+      (4, "Kevin", "Tupik",  4, 3),
+      (5, "Kunal", "Singh",  5, NULL),
+      (6, "Malia", "Brown",  6, 5),
+      (7, "Sarah", "Lourd",  7, NULL),
+      (8, "Tom", "Allen",  8, 7)
+      
